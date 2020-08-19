4d36b31365f842ca5f52e0ccdb73fce209911031
omer-mohamed <omer.javed26@gmail.com>
2020-08-19 14:22:57 -0400

    main scraper: added downloading of images

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import json
+import urllib.request
 from bs4 import BeautifulSoup
 
-def scrape_product(content):
+def scrape_product(content,index):
+    if(index == 38 or index == 39):
+        return
     soup = BeautifulSoup(content, 'html.parser')
 
     name = soup.find('h3') 
@@ -19,11 +22,16 @@ def scrape_product(content):
     details = details.replace("<br/>",",")
     details = details.split(",")
 
-    print(name," ",category," ", details)
+    img = soup.findAll("div", {"class":"overlay-container"})
+    img = "http://www.sandstaruae.com/"+img[0].img['src']
+    if index<=37:
+        urllib.request.urlretrieve(img, "./product img/%d.jpg" % index)
+        img_link = "./product img/%d.jpg" % index
+        print(img_link)
+        
+    print(name,category, details)
 
-
-
-for i in range(1,42):
+for i in range(1,41):
     URL = 'http://www.sandstaruae.com/productdetails.aspx?pid={}'.format(i)
     page = requests.get(URL)
-    scrape_product(page.content)
+    scrape_product(page.content,i)
